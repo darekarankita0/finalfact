e2695fda5e123946728dd983337241deed601254
darekarankita0 <55353280+darekarankita0@users.noreply.github.com>
2021-02-09 01:27:40 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,13 @@
 from flask import Flask, render_template, request, jsonify
+from flask_cors import CORS, cross_origin
 import json
 import requests
 import pandas as pd
 import random
 
-app = Flask(__name__)
+app = Flask(_name_)
+cors = CORS(app)
+app.config['CORS_HEADERS'] = 'Content-Type'
 
 @app.route('/', methods=['GET','POST'])
 def facts():
@@ -18,10 +21,10 @@ def facts():
     data = json.dumps(data)
     return jsonify(data)
 
-if __name__=='__main__':
+if _name=='main_':
     #server = Server(app.wsgi_app)
     #server.serve()
     #server.watch('/Views/*')
-    app.run(debug=False)
+    app.run(debug=True)
     #app.config['DEBUG'] = True
     #app.config['TEMPLATES_AUTO_RELOAD'] = True
